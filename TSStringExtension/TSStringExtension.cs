3097c41dac3ebb6fd61347a0586cf383d45b9dc6
kyoangel <gd92x2@hotmail.com>
2017-07-28 23:13:42 +0800

    [upload project] upload VS2015 C# project for practice extensionMethod

---
+++ TSStringExtension/TSStringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSStringExtension
+{
+    public static class TSStringExtension
+    {
+        public static string MaskPhoneBySymbol(this string str, char symbol)
+        {
+            string result = string.Empty;
+            if (str.Length!=10)
+            {
+                return str;
+            }
+            return result;
+        }
+    }
+}
---
+++ TSStringExtensionTests/TSStringExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TSStringExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSStringExtension.Tests
+{
+    [TestClass()]
+    public class TSStringExtensionTests
+    {
+        [TestMethod()]
+        public void PhoneNumLengthNotEqualsTo10_Should_DoNotThing()
+        {
+            string phoneNum = "123456";
+            Assert.AreEqual(phoneNum, phoneNum.MaskPhoneBySymbol(' '));
+        }
+    }
+}
